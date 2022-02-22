f31b170eea80909bfc0139ba4572a9c8e72738d3
Trey Dempsey <trey.dempsey@gmail.com>
2022-02-22 14:29:33 -0600

    Improved ByteStream performance
    * Now batches up to 100 records before yielding to the client
    * Tested performance on my machine improved from 11MiBps to 18MiBps.
    * Also generate valid json, wrapping the output in an array.
    * Used manual output of prefix '[' and suffix ']' rather than serde's
      SerializeSeq in order to avoid additional allocations and some
      trickiness with lifetimes.
    * Handle corner cases correctly, 0 records, 99, 100, and 101 records,
      the last batch of records sent before completing.

--- src/byte_stream.rs
+++ src/byte_stream.rs
@@ -18,7 +18,15 @@ pub struct ByteStream<'s, R> {
     results: u32,
     stream: Option<BoxStream<'s, Result<R, sqlx::Error>>>,
     buf: Vec<u8>,
+    state: StreamState,
     _pinned: PhantomPinned,
+}
+
+enum StreamState {
+    Unstarted,
+    FirstRecord,
+    AdditionalRecords,
+    Closed,
 }
 
 impl<'s, R> ByteStream<'s, R>
@@ -30,13 +38,13 @@ where
         sql: &str,
         results: Option<u32>,
     ) -> Pin<Box<ByteStream<'s, R>>> {
-        println!("ByteStream::new()");
         let wrapped = ByteStream {
             pool: UnsafeCell::new(pool.clone()),
             results: results.or(Some(10)).unwrap(),
             sql: UnsafeCell::new(Pin::new(String::from(sql))),
             stream: None,
             buf: Vec::new(),
+            state: StreamState::Unstarted,
             _pinned: PhantomPinned,
         };
         let mut wrapped = Box::pin(wrapped);
@@ -54,7 +62,7 @@ where
                 .fetch(pool),
         );
 
-        // Safety: We haven't returned our new instance of HoldUp yet,
+        // Safety: We haven't returned our new instance of Self yet,
         // therefore we have no references. Exclusive access is safe.
         unsafe {
             let mut_ref = Pin::get_unchecked_mut(wrapped.as_mut());
@@ -72,23 +80,64 @@ where
     type Item = Result<Bytes, Box<dyn Error>>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        // Safety: XXX What makes this use safe? XXX
+        // Safety: This is safe because we are Pin and our fields are Pin. None of our
+        // fields are public so we can guarantee our fields won't move.
         let this = unsafe { self.get_unchecked_mut() };
+
+        if matches!(this.state, StreamState::Closed) {
+            return Poll::Ready(None);
+        } else if matches!(this.state, StreamState::Unstarted) {
+            this.buf.append(&mut vec![b'[']);
+            this.state = StreamState::FirstRecord;
+        }
+
         if let Some(stream) = &mut this.stream {
-            match stream.as_mut().poll_next(cx) {
-                Poll::Pending => Poll::Pending,
-                Poll::Ready(Some(Ok(model))) => {
-                    let res = serde_json::to_writer(&mut this.buf, &model);
-                    if let Err(e) = res {
-                        return Poll::Ready(Some(Err(Box::new(e))));
+            let mut count = 0;
+
+            // Fill this.buf with up to 100 rows
+            let mut poll = stream.as_mut().poll_next(cx);
+            while let Poll::Ready(Some(Ok(ref model))) = poll {
+                // For every additional record after the first precede it with a comma
+                if matches!(this.state, StreamState::FirstRecord) {
+                    this.state = StreamState::AdditionalRecords;
+                } else {
+                    this.buf.append(&mut vec![b',']);
+                }
+
+                let res = serde_json::to_writer(&mut this.buf, &model);
+                if let Err(e) = res {
+                    return Poll::Ready(Some(Err(Box::new(e))));
+                }
+
+                count += 1;
+                if count == 100 {
+                    break;
+                }
+
+                poll = stream.as_mut().poll_next(cx);
+            }
+
+            match poll {
+                Poll::Ready(Some(Ok(_))) | Poll::Ready(None) | Poll::Pending if count != 0 => {
+                    if matches!(poll, Poll::Ready(None)) {
+                        this.state = StreamState::Closed;
+                        this.buf.append(&mut vec![b']']);
                     }
 
                     let poll = Poll::Ready(Some(Ok(Bytes::copy_from_slice(&this.buf))));
                     this.buf.clear();
                     poll
                 }
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(None) => {
+                    this.state = StreamState::Closed;
+                    this.buf.append(&mut vec![b']']);
+                    let poll = Poll::Ready(Some(Ok(Bytes::copy_from_slice(&this.buf))));
+                    this.buf.clear();
+                    poll
+                }
                 Poll::Ready(Some(Err(e))) => Poll::Ready(Some(Err(Box::new(e)))),
-                Poll::Ready(None) => Poll::Ready(None),
+                Poll::Ready(Some(Ok(_))) => unreachable!(),
             }
         } else {
             Poll::Ready(Some(Err(Box::new(io::Error::new(
